a888b21068c798168e91278f23c40c7903a40ac8
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2016-12-03 08:38:02 -0500

    Added JobTrial builder ReturnFrom

--- src/Chessie.Hopac/Library.fs
+++ src/Chessie.Hopac/Library.fs
@@ -1,16 +1,72 @@
 namespace Chessie.Hopac
 
-/// Documentation for my library
-///
-/// ## Example
-///
-///     let h = Library.hello 1
-///     printfn "%d" h
-///
-module Library = 
+open Chessie.ErrorHandling
+open Hopac
+open System.Threading.Tasks
+
+[<NoComparison;NoEquality>]
+type JobResult<'a, 'b> = 
+    | JobResult of Job<Result<'a, 'b>>
+
+
+[<AutoOpen>]
+module JobTrial =
+
+    let inline ofValue value =
+        value
+        |> ok 
+        |> Job.result
+        |> JobResult
+
+    let inline ofJobOfResult (value : Job<Result<'a,'b>>) =
+        value
+        |> JobResult
+
+    let inline ofResult (value : Result<'a,'b>) = 
+        value
+        |> Job.result
+        |> ofJobOfResult
+
+    let inline ofAsyncResult (value : AsyncResult<'a,'b>) =
+        value
+        |> Async.ofAsyncResult
+        |> Job.fromAsync
+        |> ofJobOfResult
+    let inline ofAsyncOfResult (value : Async<Result<'a,'b>>) =
+        value
+        |> Job.fromAsync
+        |> ofJobOfResult
+
+    let inline ofTaskOfResult (value : Task<Result<'a,'b>>) =
+        value
+        |> Job.awaitTask
+        |> ofJobOfResult
+
+    let inline ofJobResult (JobResult jr) = jr
+
+    type JobTrialBuilder () =
+      member __.Return value : JobResult<'a,'b> =
+       value
+       |> ofValue 
+      member __.ReturnFrom (value : JobResult<'a,'b>) = value
+      member __.ReturnFrom (value : Job<Result<'a,'b>>) = 
+        value
+        |> ofJobOfResult
+      member __.ReturnFrom (value : Result<'a,'b>) = 
+        value
+        |> ofResult
+
+      member __.ReturnFrom (value : AsyncResult<'a,'b>) = 
+        value
+        |> ofAsyncResult
+      member __.ReturnFrom (value : Async<Result<'a,'b>>) = 
+        value
+        |> ofAsyncOfResult
+
+      member __.ReturnFrom (value : Task<Result<'a,'b>>) = 
+        value
+        |> ofTaskOfResult
   
-  /// Returns 42
-  ///
-  /// ## Parameters
-  ///  - `num` - whatever
-  let hello num = 42
+    let jobTrial = JobTrialBuilder()
+    
+  
--- tests/Chessie.Hopac.Tests/Tests.fs
+++ tests/Chessie.Hopac.Tests/Tests.fs
@@ -2,9 +2,63 @@ module Chessie.Hopac.Tests
 
 open Chessie.Hopac
 open NUnit.Framework
+open Hopac
+open Chessie.ErrorHandling
+open System.Threading.Tasks
+
+let getOkValue (jr : JobResult<'a,'b>) =
+  jr 
+  |> JobTrial.ofJobResult
+  |> Hopac.run
+  |> Trial.returnOrFail
+
 
 [<Test>]
-let ``hello returns 42`` () =
-  let result = Library.hello 42
-  printfn "%i" result
+let ``Computation Expreession Returns value`` () =
+  let return42 = jobTrial { return 42 }
+  let result = return42 |> getOkValue
+  Assert.AreEqual(42,result)
+
+let ``Computation Expreession Returns JobResult`` () =
+  let returnsReturn = jobTrial{
+    return!  jobTrial { return 42 }
+  }
+  let result = returnsReturn |> getOkValue
+  Assert.AreEqual(42,result)
+
+let ``Computation Expreession Returns JobOfResult`` () =
+
+  let returnsReturn = jobTrial{
+    return! job { return ok 42 } 
+  }
+  let result = returnsReturn |> getOkValue
+  Assert.AreEqual(42,result)
+
+let ``Computation Expreession Returns Result`` () =
+  let returnsReturn = jobTrial{
+    return! ok 42 
+  }
+  let result = returnsReturn |> getOkValue
+  Assert.AreEqual(42,result)
+
+
+let ``Computation Expreession Returns AsyncResult`` () =
+  let returnsReturn = jobTrial{
+    return! asyncTrial { return 42 }  
+  }
+  let result = returnsReturn |> getOkValue
+  Assert.AreEqual(42,result)
+
+let ``Computation Expreession Returns AsyncOfResult`` () =
+  let returnsReturn = jobTrial{
+    return! async { return ok 42 }  
+  }
+  let result = returnsReturn |> getOkValue
+  Assert.AreEqual(42,result)
+
+let ``Computation Expreession Returns TaskOfResult`` () =
+  let returnsReturn = jobTrial{
+    return! Task.FromResult(ok 42)
+  }
+  let result = returnsReturn |> getOkValue
   Assert.AreEqual(42,result)
